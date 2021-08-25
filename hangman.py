aeeac57410429f274ed337547dbf4767c8b41cce
Vinicius Serpa <vinicius-serpa@live.com>
2021-08-25 00:59:51 -0300

    add hit and hanged control

--- hangman.py
+++ hangman.py
@@ -6,18 +6,30 @@ secret_word = 'apple'
 correct_letters = ['_', '_', '_', '_', '_']
 hit = False
 hanged = False
+errors = 0
 
 print(correct_letters)
 while (not hit and not hanged):
     guess = input('Letter? ')
 
-    pos = 0
-    for letter in secret_word:
-        if (guess.upper() == letter.upper()):
-            correct_letters[pos] = letter
-            print('I found the letter "{}" in position {}'.format(letter, pos + 1))
-        pos = pos + 1
+    if (guess in secret_word):
+        pos = 0
+        for letter in secret_word:
+            if (guess.upper() == letter.upper()):
+                correct_letters[pos] = letter
+                print('I found the letter "{}" in position {}'.format(letter, pos + 1))
+            pos = pos + 1
+    else:
+        errors += 1
         
+    hanged = errors == 6
+    hit = '_' not in correct_letters
+    
     print(correct_letters)
+
+if (hit):
+    print('You win!')
+else:
+    print('You loose!')
 
 print('Game Over')
