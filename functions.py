eeb80f1e03186004dfdc39c152ed0bb8e4f19bbe
Vinicius Serpa <vinicius-serpa@live.com>
2021-08-25 12:11:53 -0300

    add args and kwargs

--- functions.py
+++ functions.py
@@ -16,6 +16,13 @@ def calc(number_1, number_2):
 def div(number_1, number_2):
     return number_1 / number_2
 
-print(average_velocity(100, 20))
-print(average_velocity(-20, 10))
-print(average_velocity(150, 0))
+def test_args_kwargs(arg1, arg2, arg3):
+    print('arg1:', arg1)
+    print('arg2:', arg2)
+    print('arg3:', arg3)
+
+args = ('one', 2, 3)
+test_args_kwargs(*args)
+
+kwargs = {'arg3': 3, 'arg2': 'two', 'arg1': 'one'}
+test_args_kwargs(**kwargs)
