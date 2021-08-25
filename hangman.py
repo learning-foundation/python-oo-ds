a84d46ba565c1adcb0b661194608c1a8b45894e9
Vinicius Serpa <vinicius-serpa@live.com>
2021-08-25 00:34:21 -0300

    add a list to persist correct letters

--- hangman.py
+++ hangman.py
@@ -3,17 +3,21 @@ print('***  Welcome to Hangman Game! ***')
 print('*********************************')
 
 secret_word = 'apple'
+correct_letters = ['_', '_', '_', '_', '_']
 hit = False
 hanged = False
 
+print(correct_letters)
 while (not hit and not hanged):
     guess = input('Letter? ')
 
     pos = 0
     for letter in secret_word:
         if (guess.upper() == letter.upper()):
-            print('I found the letter {} in position {}'.format(letter, pos + 1))
+            correct_letters[pos] = letter
+            print('I found the letter "{}" in position {}'.format(letter, pos + 1))
         pos = pos + 1
-        print('Playing...')
+        
+    print(correct_letters)
 
 print('Game Over')
