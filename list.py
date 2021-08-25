5a5c226258b456b9a9508d0d2aba5b686eb90cf3
Vinicius Serpa <vinicius-serpa@live.com>
2021-08-25 09:54:40 -0300

    refactoring: list exercises with one loop

--- list.py
+++ list.py
@@ -1,40 +1,47 @@
 my_list = [12, -2, 4, 8, 29, 45, 78, 36, -17, 2, 12, 8, 3, 3, -52]
 
-# show max element: 78
-print('max: ' + str(max(my_list)))
+first_element = my_list[0]
+max_element = first_element
+min_element = first_element
+even_list = []
+odd_list = []
+first_element_occurrences = 0
+sum_of_elements = 0
+average = 0
+sum_of_negative_values = 0
+my_list_len = 0
 
-# show min element: -52
-print('min: ' + str(min(my_list)))
+for index in range(0, len(my_list)):
 
-# show even numbers: 12, -2, 4, 8, 78, 36, 2, 12, 8, -52
-print('even')
-for element in my_list:
+    element = my_list[index]
+    sum_of_elements += element
+    my_list_len += 1
+
+    if (element > max_element):
+        max_element = element
+
+    if (element < min_element):
+        min_element = element
+
     if (element % 2 == 0):
-        print(element)
+        even_list.append(element)
+    else:
+        odd_list.append(element)
 
-# show odd numbers: 29, 45, -17, 3, 3
-print('odd')
-for element in my_list:
-    if (element % 2 != 0):
-        print(element)
+    if (element == first_element):
+        first_element_occurrences += 1
 
-# show occurrences of first element: 2
-first_element_occurrences = my_list.count(my_list[0])
-print('first element occurrences: ' + str(first_element_occurrences))
-
-# show average: 11.2666...
-sum_of_elements = 0
-for element in my_list:
-    sum_of_elements = sum_of_elements + element
-average = sum_of_elements / len(my_list)
-print('average: ' + str(average))
-
-# show sum of negative values: -71
-sum_of_elements = 0
-for element in my_list:
     if (element < 0):
-        sum_of_elements += element
-print('sum of negative elements: ' + str(sum_of_elements))
+        sum_of_negative_values += element
+    
+
+average = sum_of_elements / my_list_len
 
 
-# refactoring
+print('max: ' + str(max_element)) # 78
+print('min: ' + str(min_element)) # -52
+print('even:' + str(even_list)) # 12, -2, 4, 8, 78, 36, 2, 12, 8, -52
+print('odd: ' + str(odd_list)) # 29, 45, -17, 3, 3
+print('first element occurrences: ' + str(first_element_occurrences)) # 2
+print('average: ' + str(average)) # 11.2666...
+print('sum of negative elements: ' + str(sum_of_negative_values)) # -71
