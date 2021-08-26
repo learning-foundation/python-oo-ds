faa6c32a144b9272194b0b0ee00162930c601ab1
Vinicius Serpa <vinicius-serpa@live.com>
2021-08-25 21:33:12 -0300

    refactor the guessing game

--- games/number.py
+++ games/number.py
@@ -1,21 +1,31 @@
-def play():
+import random
+
+def start_message():
     print('******************************')
     print('*    Number Guess Game       *')
     print('******************************')
 
-    secret_number = 60
-    level = 0
+def round_message(round_number, total_guess, guess_string):
+    print('Trying {} of {}'.format(round_number, total_guess))
+    print('Do you type ' + guess_string)
 
+def choose_level():
+    level = 0
     while (level < 1 or level > 3):
         level = int(input('Choose a dificult level from 1 to 3: '))
+    return level
 
+def play():
+    start_message()
+    secret_number = random.randrange(1, 101)
+    level = choose_level()
     total_guess = 20 - (level * 5)
     points = 900 + (100 * level)
 
     for round_number in range(1, total_guess + 1):
-        print('Trying {} of {}'.format(round_number, total_guess))
+
         guess_string = input('Type a number from 1 to 100\n')
-        print('Do you type ' + guess_string)
+        round_message(round_number, total_guess, guess_string)
 
         guess = int(guess_string)
         win = guess == secret_number
@@ -27,8 +37,14 @@ def play():
             print('Your pontuation is {}'.format(points))
             break
         elif (major):
-            print('Wrong, try a smaller number')
+            if (round_number != total_guess):
+                print('Wrong, try a smaller number')
+            else:
+                print('Wrong')
             points = points - (guess - secret_number)
         elif (minor):
-            print('Wrong, try a bigger number')
+            if (round_number != total_guess):
+                print('Wrong, try a bigger number')
+            else:
+                print('Wrong')
             points = points - (secret_number - guess)
