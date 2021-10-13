58c212da4562f65f99c8df24bff7667744ea552b
Vinicius Serpa <vinicius-serpa@live.com>
2021-10-12 21:15:26 -0300

    include type checkin

--- collections/container.py
+++ collections/container.py
@@ -1,4 +1,5 @@
 from collections.abc import MutableSequence
+from employee import Employee
 
 class Employees(MutableSequence):
 
@@ -11,13 +12,19 @@ class Employees(MutableSequence):
         return self._data[pos]
 
     def __setitem__(self, pos, value):
-        self._data[pos] = value
+        if (isinstance(value, Employee)):
+            self._data[pos] = value
+        else:
+            raise TypeError('Only Employee is accepted as a value')
 
     def __delitem__(self, pos):
         del self._data[pos]
 
     def insert(self, pos, value):
-        return self._data.insert(pos, value)
+        if (isinstance(value, Employee)):
+            return self._data.insert(pos, value)
+        else:
+            raise TypeError('Only Employee is accepted as a value')
 
 if __name__ == '__main__':
     employess = Employees()
